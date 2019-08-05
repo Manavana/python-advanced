de8b912470936874e6b7e1d6324872179b38a21f
Manavana <stivrina.maria@gmail.com>
2019-08-05 23:28:02 +0700

    Описание прослушивания клиента. Описание обработки запросов

--- server/__main__.py
+++ server/__main__.py
@@ -8,6 +8,12 @@ port = PORT
 try:
     sock = socket.socket()
     sock.bind((host, port))
+    sock.listen(5)
+
     print('Server started')
+
+    while True:
+        client, address = sock.accept()
+        print(f'Client with address { address } was detected')
 except KeyboardInterrupt:
     print('Server closed')
