ff682e48f235121ad0d776f2199904d1e9a78911
Manavana <stivrina.maria@gmail.com>
2019-08-05 23:54:24 +0700

    Реализован парсер аргументов, в случае необходимости переопределения констант из файла концигураций

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,7 @@
 import json
 import socket
+import yaml
+import argparse
 
 from settings import HOST, PORT, BUFFERSIZE, ENCODIND
 
@@ -7,6 +9,24 @@ host = HOST
 port = PORT
 buffersize = BUFFERSIZE
 encoding = ENCODIND
+
+# описываем парсер аргументов
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-c', '--config', type=str,
+    help='Sets run configuration'
+)
+args = parser.parse_args()
+
+# если внутри аргументов args обнаружится config, то произойдет определение файла конфигураций, извлечение его значений
+# и переопределение констант
+if args.config:
+    with open(args.config) as file:
+        conf = yaml.load(file, Loader=yaml.Loader)
+        host = conf.get('host', HOST)
+        port = conf.get('port', PORT)
+        buffersize = conf.get('buffersize', BUFFERSIZE)
+        encoding = conf.get('encoding', ENCODING)
 
 try:
     sock = socket.socket()
@@ -20,10 +40,8 @@ try:
         print(f'Client with address { address } was detected')
 
         b_data = client.recv(buffersize)
-
         request = json.loads(b_data.decode(encoding))
         response = json.dumps(request)
-
         client.send(response.encode(encoding))
 
 except KeyboardInterrupt:
