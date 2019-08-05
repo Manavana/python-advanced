9c399250187a804f6a43ca298050e4b356e69c5b
Manavana <stivrina.maria@gmail.com>
2019-08-05 23:22:26 +0700

    Создали сокет и привязку к хосту и порту сервера-клиента.

--- server/__main__.py
+++ server/__main__.py
@@ -1,8 +1,13 @@
 import socket
 
+from settings import HOST, PORT
+
+host = HOST
+port = PORT
 
 try:
     sock = socket.socket()
+    sock.bind((host, port))
     print('Server started')
 except KeyboardInterrupt:
     print('Server closed')
