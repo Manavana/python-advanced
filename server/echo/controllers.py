a7fd9e1015e90801e9f9b103d90ca18fc23697b5
Manavana <stivrina.maria@gmail.com>
2019-08-12 22:46:01 +0700

    Добавлены комментарии

--- server/echo/controllers.py
+++ server/echo/controllers.py
@@ -2,5 +2,7 @@ from protocol import make_response
 
 
 def get_echo(request):
+    # из объекта запроса извлекаются данные
     data = request.get('data')
+    # возвращаем объект запроса, основанные на его данных: запрос, код ответа сервера, данные
     return make_response(request, 200, data)
